90abe24168a07670cd250d7b3cd6111da48ef1ba
aidcoope <aidcoope@iu.edu>
2024-01-13 20:15:53 -0500

    Minimum population feature added, added India to allowed countries and modified csv to work with minpop feature.

--- GeoguessrTopCityGenerator/Program.cs
+++ GeoguessrTopCityGenerator/Program.cs
@@ -63,38 +63,60 @@ namespace GeoguessrTopCityGenerator
 
         static void Main(string[] args)
         {
+            // Change to getDevFilePath() if testing within visual studio
             string filePath = getDevFilePath();
             Console.WriteLine("Welcome! This program will generate a json file for importing into map-making.app that will contain the most populous cities of every geoguessr valid country. Make sure to put your csv file into the same directory as this program.");
             Console.WriteLine("How many cities per country do you want to generate?");
             int cityCount = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What is the minimum population a city can have for it to be included? (Set to -1 to include all cities, including those with no population listed)");
+            int minPopulation = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Generating " + cityCount + " cities for each country...");
-            generateMapJson(filePath, cityCount);
+            generateMapJson(filePath, cityCount, minPopulation);
             Console.WriteLine("Generation is complete, the json will now be in the application's directory.");
         }
 
-        static private async void generateMapJson(string filePath, int cityCount)
+        static private async void generateMapJson(string filePath, int cityCount, int minPopulation)
         {
             Dictionary<string, int> remainingCitiesPerCountry = generateCityCountDictionary(filePath, cityCount);
             List<CustomCoordinate> coordinates = new List<CustomCoordinate>();
-            // Operates under the assumption that the CSV file is under the same format as the one from https://simplemaps.com/data/world-cities
+            // Operates under the assumption that the CSV file is under the same format as the one from https://simplemaps.com/data/world-cities. This sheet is already sorted by population.
             bool pastFirstLine = false;
             foreach (string line in File.ReadLines(filePath + citiesFileName))
             {
                 if (pastFirstLine)
                 {
                     string[] cityInfo = line.Split(',');
+                    Console.WriteLine(cityInfo[0] + " / " + cityInfo[1] + " / " + cityInfo[2] + " / " + cityInfo[3] + " / " + cityInfo[8] + cityInfo[9]);
                     string cityName = cityInfo[1]; // Get's cities ascii name for tags
                     string countryName = cityInfo[4];
                     // Remove quotation marks and convert to double
-                    double cityLattitude = Convert.ToDouble(cityInfo[2].Substring(1, cityInfo[2].Length - 2)); //cityInfo[2].Substring(1, cityInfo[2].Length - 2)
-                    double cityLongitude = Convert.ToDouble(cityInfo[3].Substring(1, cityInfo[3].Length - 2));
+                    double cityLattitude = Convert.ToDouble(cityInfo[2]); //cityInfo[2].Substring(1, cityInfo[2].Length - 2)
+                    double cityLongitude = Convert.ToDouble(cityInfo[3]);
+                    int population;
+                    // If no population is listed...
+                    if (cityInfo[9].Equals(""))
+                    {
+                        population = -1;
+                    }
+                    else
+                    {
+                        population = Convert.ToInt32(cityInfo[9]);
+                    }
                     int remainingCities;
                     if (remainingCitiesPerCountry.TryGetValue(countryName, out remainingCities))
                     {
                         if (remainingCities > 0)
                         {
-                            coordinates.Add(new CustomCoordinate(cityLattitude, cityLongitude, countryName, cityName, cityCount - (remainingCities - 1)));
-                            remainingCitiesPerCountry[countryName] = remainingCities - 1;
+                            if (population >= minPopulation)
+                            {
+                                coordinates.Add(new CustomCoordinate(cityLattitude, cityLongitude, countryName, cityName, cityCount - (remainingCities - 1)));
+                                remainingCitiesPerCountry[countryName] = remainingCities - 1;
+                            }
+                            // Once a city's population is found that is below the minimum, stop searching, as all cities below it will have lower population.
+                            else 
+                            {
+                                remainingCitiesPerCountry[countryName] = 0;
+                            }
                         }
                         // Otherwise city excluded as max cities reached
                     }
