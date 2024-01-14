3c01f97bf0694b886cdbdfbfd97ca2385e641d6c
aidcoope <aidcoope@iu.edu>
2024-01-13 20:25:59 -0500

    Removed debug print statements.

--- GeoguessrTopCityGenerator/Program.cs
+++ GeoguessrTopCityGenerator/Program.cs
@@ -65,7 +65,7 @@ namespace GeoguessrTopCityGenerator
         {
             // Change to getDevFilePath() if testing within visual studio and getAppFilePath() for compilation.
             string filePath = getAppFilePath();
-            Console.WriteLine("Welcome! This program will generate a json file for importing into map-making.app that will contain the most populous cities of every geoguessr valid country. Make sure to put your csv file into the same directory as this program.");
+            Console.WriteLine("Welcome! This program will generate a json file for importing into map-making.app that will contain the most populous cities of every geoguessr valid country.\nAfter the process runs and closes, import coordinates.json into map-making.app");
             Console.WriteLine("How many cities per country do you want to generate?");
             int cityCount = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("What is the minimum population a city can have for it to be included? (Set to -1 to include all cities, including those with no population listed)");
@@ -73,6 +73,7 @@ namespace GeoguessrTopCityGenerator
             Console.WriteLine("Generating " + cityCount + " cities for each country...");
             generateMapJson(filePath, cityCount, minPopulation);
             Console.WriteLine("Generation is complete, the json will now be in the application's directory.");
+
         }
 
         static private async void generateMapJson(string filePath, int cityCount, int minPopulation)
@@ -86,7 +87,6 @@ namespace GeoguessrTopCityGenerator
                 if (pastFirstLine)
                 {
                     string[] cityInfo = line.Split(',');
-                    Console.WriteLine(cityInfo[0] + " / " + cityInfo[1] + " / " + cityInfo[2] + " / " + cityInfo[3] + " / " + cityInfo[8] + cityInfo[9]);
                     string cityName = cityInfo[1]; // Get's cities ascii name for tags
                     string countryName = cityInfo[4];
                     // Remove quotation marks and convert to double
@@ -138,7 +138,6 @@ namespace GeoguessrTopCityGenerator
             Dictionary<string, int> cityPerCountry = new Dictionary<string, int>();
             foreach (string country in File.ReadAllText(filePath + countriesFileName).Split(','))
             {
-                Console.WriteLine("Added country " + country + " to allowed list.");
                 cityPerCountry[country] = cityCount;
             }
             return cityPerCountry;
